3e1a22e4857d3999bb84f669417b597acee598b0
IreneGaoc <zgao1@ualberta.ca>
2019-01-24 12:56:34 -0700

    assignment1 done most of the parts

--- README.md
+++ README.md
@@ -17,9 +17,26 @@ server.py contains contributions from:
 * Abram Hindle
 * Eddie Antonio Santos
 * Jackson Z Chang
+* Mandy Meindersma
 
 But the server.py example is derived from the python documentation
 examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
 
+
+Created by
+========================
+
+Irene Gao
+
+Collaboration
+========================
+
+I consulted to Xinyi Zhuang
+
+Reference
+========================
+* https://stackoverflow.com/questions/38412495/difference-between-os-path-dirnameos-path-abspath-file-and-os-path-dirnam
+* https://stackoverflow.com/questions/16904780/python-socket-and-socketserver
+* https://stackoverflow.com/questions/15433188/r-n-r-and-n-what-is-the-difference-between-them
--- server.py
+++ server.py
@@ -1,14 +1,14 @@
-#  coding: utf-8 
+#  coding: utf-8
 import socketserver
-
+import os
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+# Copyright 2019 Irene Gao
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,11 +28,56 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
+
     def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        self.data = str(self.request.recv(1024).strip(),'utf-8')
+        #print ("Got a request of: %s\n" % self.data)
+        # initialize
+
+        method = self.data.split()[0]
+        url = self.data.split()[1]
+        cur_path = os.path.dirname(os.path.abspath(__file__)) + "/www"
+        abs_path = cur_path + url
+        resp = " "
+        print(url)
+        if method == 'GET':
+            if os.path.isfile(abs_path):
+                if '.html' in abs_path:
+                    openfile = open(abs_path).read()
+                    resp = "HTTP/1.1 200 OK contents of \n" + "Content-Type: text/html\n" + "\n"
+                    final = resp + openfile
+                elif '.css' in abs_path:
+                    openfile = open(abs_path).read()
+                    resp = "HTTP/1.1 200 OK contents of \n" + "Content-Type: text/css\n" + "\n"
+                    final = resp + openfile
+                else:
+                    resp = "HTTP/1.1 404 Not Found\n" + "Content-Type: text/plain\n" + "\n"
+                    final = resp
+            elif os.path.isdir(abs_path):
+                if os.path.isfile(abs_path+ "index.html"):
+                    openfile =  open(abs_path + "index.html").read()
+                    resp = "HTTP/1.1 200 OK contents of \n" + "Content-Type: text/html\n" + "\n"
+                    final = resp + openfile
+                elif url[-1:] == "/":
+                    openfile = open(abs_path).read()
+                    resp = "HTTP/1.1 200 OK contents of \n" + "Content-Type: text/html\n" + "\n"
+                    final = resp + openfile
+                elif url[-1:] != "/":
+                    resp = "HTTP/1.1 301 Permanently moved to \n" + "Content-Type: text/html\n" + "\n"
+                    final = resp
+                else:
+                    resp = "HTTP/1.1 404 Not Found\n" + "Content-Type: text/plain\n" + "\n"
+                    final = resp
+            else:
+                resp = "HTTP/1.1 404 Not Found\n" + "Content-Type: text/plain\n" + "\n"
+                final = resp
+        else:
+            resp = "HTTP/1.1 405 Method Not Allowed\n" + "Content-Type: text/plain\n" + "\n"
+            final = resp
+
+        print(final)
+        self.request.sendall(bytearray(final,'utf-8'))
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
